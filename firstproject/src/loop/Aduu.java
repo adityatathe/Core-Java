65b107d0f50c294c1f5705e4f26292ec9a72b76e
Admin <Admin@DESKTOP-FRH0ASL>
2025-09-03 23:58:01 +0530

    ifelse

---
+++ firstproject/src/loop/Aduu.java
@@ -0,0 +1,21 @@
+package loop;
+
+public class Aduu {
+	public static void main(String[] args) {
+		
+		int i=1;
+		System.out.println("this are whole numbers");
+		
+		for(i=1;i<10;i++)
+		{
+			
+				
+				
+		
+			System.out.println(i);
+		
+			
+		}
+	}
+
+}
